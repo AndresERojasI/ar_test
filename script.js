42d3aca0719c6aac674719391dbec20512285241
Andres Rojas <andres.rojas.isaza@gmail.com>
2023-02-15 06:54:44 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("vidhandler", {
+  // ...
+  init: function () {
+    // Set up initial state and variables.
+    this.toggle = false;
+    this.vid = document.querySelector("#video");
+    this.vid.pause();
+  },
+  tick: function () {
+    if (this.el.object3D.visible == true) {
+      if (!this.toggle) {
+        this.toggle = true;
+        this.vid.play();
+      }
+    } else {
+      this.toggle = false;
+      this.vid.pause();
+    }
+  },
+});
